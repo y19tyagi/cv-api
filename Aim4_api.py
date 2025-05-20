fcf736e82aa4fd87bc65a1d2980215f2d333b383
y19tyagi <yashtyagi953@gmail.com>
2025-05-20 10:18:26 +0200

    fix: use correct host and port for Render

--- Aim4_api.py
+++ Aim4_api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from docx import Document
+import os
 import base64
 import io
 import zipfile
@@ -55,6 +56,10 @@ def extract():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
